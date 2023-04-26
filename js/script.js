742b6c89e563d0a2b75229990d9c91101ce10930
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-04-26 17:12:47 +0200

    finito senza bonus

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ const gioca = document.getElementById("play");
 play.addEventListener("click", function() {
     containerr.innerHTML="";
     for (let i = 1; i < 101; i++) {
-       const x = `<div class="box">${i}</div>`;
+        const x = document.createElement("div");
+        x.classList.add("box");
+        x.classList.add("onclick")
+        x.innerHTML+=`${i}`
         x.addEventListener("click", function() {
-            x.classList.add("onclick")
             this.classList.toggle("onclick");
             console.log(this)
       });
-      containerr.innerHTML += x;
+      containerr.appendChild(x)
     }
     return
 });
